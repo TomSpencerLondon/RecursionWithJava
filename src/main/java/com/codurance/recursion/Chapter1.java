02d70673fbbca80ab455fc14db297928d5ed2ac0
Tom <thomas.spencer@codurance.com>
2021-02-14 10:20:46 +0000

    add module 1

--- src/main/java/com/codurance/recursion/Chapter1.java
+++ src/main/java/com/codurance/recursion/Chapter1.java
@@ -1,5 +1,11 @@
 package com.codurance.recursion;
 
+import net.bytebuddy.dynamic.scaffold.MethodGraph;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 public class Chapter1 {
   public static void main(String[] args) {
     printNumbers(10);
@@ -12,6 +18,11 @@ public class Chapter1 {
     System.out.println(countEvensInArray(new int[]{1, 2, 3, 4, 5, 6}));
 
     System.out.println(fibonacci(8));
+
+    printReversedLinkedList(new Node(1, new Node(2, new Node(3, new Node(4, null)))));
+    printReversedLinkedListRecursive(new Node(1, new Node(2, new Node(3, new Node(4, null)))));
+
+    System.out.println(combinations(new int[]{1, 2, 3}));
   }
 
   public static void printNumbers(int n) {
@@ -73,5 +84,56 @@ public class Chapter1 {
     if (x <= 0) return 0;
     if (x == 1) return 1;
     return fibonacci(x - 1) + fibonacci(x - 2);
+  }
+
+  public static void printReversedLinkedListRecursive(Node head){
+    if (head == null) return;
+    printReversedLinkedListRecursive(head.next);
+    System.out.println(head.val);
+  }
+
+  public static void printReversedLinkedList(Node head){
+    Stack<Node> s = new Stack<Node>();
+    while (head != null) {
+      s.push(head);
+      head = head.next;
+    }
+    while(!s.isEmpty()){
+      System.out.println(s.pop());
+    }
+  }
+
+  public static List<List<Integer>> combinations(int[] n){
+    List<List<Integer>> results = new LinkedList<>();
+    combinationsPassed(n, 0, results, new LinkedList<Integer>());
+    return results;
+  }
+
+  private static void combinationsPassed(int[] n, int i, List<List<Integer>> results, List<Integer> path){
+    if (i == n.length){
+      results.add(path);
+      return;
+    }
+
+    List<Integer> pathWithCurrent = new LinkedList<>(path);
+    pathWithCurrent.add(n[i]);
+
+    combinationsPassed(n, i+1, results, path);
+    combinationsPassed(n, i + 1, results, pathWithCurrent);
+
+  }
+}
+
+class Node {
+  int val;
+  Node next;
+
+  public Node(int val, Node next){
+    this.val = val;
+    this.next = next;
+  }
+
+  public String toString(){
+    return val + "";
   }
 }
---
+++ src/main/java/com/codurance/recursion/module1/Module1.java
@@ -0,0 +1,66 @@
+package com.codurance.recursion.module1;
+
+import net.bytebuddy.dynamic.scaffold.MethodGraph;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Module1 {
+  public static void main(String[] args) {
+    System.out.println(f1(5));
+    System.out.println(f2(4));
+    System.out.println(f3(new int[]{1,2,3}));
+    System.out.println(f4(2, 6));
+  }
+
+  public static int f1(int N){
+    if (N < 0) return 0;
+    if (N == 0) return 1;
+    return f1(N-1) + f1(N-2) + f1(N-3);
+  }
+
+  public static int f2(int N){
+    if (N <= 1) return 1;
+    int total = 0;
+    for (int i = 0; i < N; i++){
+      total += f2(i) * f2(N - i - 1);
+    }
+    return total;
+  }
+
+  public static List<List<Integer>> f3(int[] n) {
+    return f3(n, 0);
+  }
+
+  private static List<List<Integer>> f3(int[] n, int i){
+    if (i == n.length) {
+      List<List<Integer>> toReturn = new LinkedList<List<Integer>>();
+      toReturn.add(new LinkedList<Integer>());
+      return toReturn;
+    }
+
+    List<List<Integer>> toReturn = new LinkedList<List<Integer>>();
+    for (List<Integer> result : f3(n, i + 1)) {
+      toReturn.add(new LinkedList<Integer>(result));
+      result.add(0, n[i]);
+      toReturn.add(new LinkedList<Integer>(result));
+    }
+
+    return toReturn;
+  }
+
+  public static int f4(int n, int m) {
+    if (m == 0) return 0;
+    if (m == 1) return 1;
+    if (n == 1) return m;
+
+    int max = Integer.MAX_VALUE;
+    for (int i = 1; i <= m; i++){
+      int case1 = f4(n - 1, i - 1);
+      int case2 = f4(n, m - i);
+      max = Math.min(max, Math.max(case1, case2));
+    }
+
+    return max + 1;
+  }
+}
