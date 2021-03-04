59a847a6f039106696bb63986343e409b8337b1d
Tom <thomas.spencer@codurance.com>
2021-03-04 13:46:40 +0000

    Add minMax

--- src/main/java/com/codurance/recursion/module2/Homework.java
+++ src/main/java/com/codurance/recursion/module2/Homework.java
@@ -1,11 +1,15 @@
 package com.codurance.recursion.module2;
 
+import java.util.Arrays;
+
 public class Homework {
 
   public static void main(String[] args) {
-    int[] arr = new int[]{2,4,6,8,10, 12};
+//    int[] arr = new int[]{2,4,6,8,10, 12};
+//
+//    printOdd(arr);
 
-    printOdd(arr);
+    System.out.println(Arrays.toString(minMax(new int[]{4,6,2,8,1,11,3})));
   }
 
   public static void printOdd(int[] arr) {
@@ -19,5 +23,25 @@ public class Homework {
 
     System.out.println(arr[counter]);
     printOdd(arr, counter + 2);
+  }
+
+  public static int[] minMax(int[] arr){
+    return minMax(arr, 0, Integer.MAX_VALUE, 0);
+  }
+
+  private static int[] minMax(int[] arr, int counter, int min, int max){
+    if (counter >= arr.length){
+      return new int[]{min, max};
+    }
+
+    if (min > arr[counter]){
+      min = arr[counter];
+    }
+
+    if (max < arr[counter]){
+      max = arr[counter];
+    }
+
+    return minMax(arr, counter + 1, min, max);
   }
 }
