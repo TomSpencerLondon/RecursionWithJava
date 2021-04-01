07d18716a3707e99f0afa6296a81746358ec820a
Tom <thomas.spencer@codurance.com>
2021-04-01 13:11:41 +0100

    Add code for moveCount tower of hanoi.

---
+++ src/main/java/com/codurance/recursion/module3/Presentation.java
@@ -0,0 +1,33 @@
+package com.codurance.recursion.module3;
+// Given n disks, find the number of
+// moves required to move all the disks
+// from position 1 to position 3
+// Only one disk can be moved at a time
+// A larger disk cannot be placed on top of a smaller disk
+// Move the n-1th disks to the aux position,
+// then move the largest, then move the n-1th disks again
+public class Presentation {
+  public static void main(String[] args) {
+    System.out.println(moveCount(4));
+  }
+  // in order to take n disks from 1 - 3 using 2
+  // move n - 1 from 1 to 2 using 3
+  // move 1 from 1 to 3
+  // move n - 1 from 2 to 3 using 1
+  public static int moveCount(int n) {
+    if (n == 1) return 1;
+    System.out.println(n);
+    return 2 * moveCount(n - 1) + 1;
+  }
+}
+
+
+// 4 - 3 - 2 - 1
+
+// 15
+
+// 2 * (m(2)) + 1
+
+// 2 * (m(1)) + 1 - 3
+
+// isPalindrome - tower of hanoi also.
